9ca6a165cef74e6fe4243b19c198869d9935488f
AlejoEA <javierecheverriaariza@gmail.com>
2026-06-03 14:34:02 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// =============================================
+//  CONFIGURACIÓN DE SUPABASE
+//  ⚠️ Reemplaza estos valores con los tuyos
+//  de https://supabase.com/dashboard
+// =============================================
+
+const SUPABASE_URL  = "https://fjoticzjzjausknegkwo.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqb3RpY3pqemphdXNrbmVna3dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MDc3ODAsImV4cCI6MjA5NjA4Mzc4MH0.Sl04eoqVUvjh9EGhVJnCwNecJKFYOFg1jrd4lnNwwIY";
